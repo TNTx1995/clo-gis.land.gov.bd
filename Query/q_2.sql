cae982a095f4f8a69736c4583518d4fbdce5d5bd
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2022-11-20 11:39:09 +0600

    query updated in style

--- Query/q_2.sql
+++ Query/q_2.sql
@@ -1,11 +1,11 @@
 WITH
-    query AS (SELECT geom AS geometry FROM borolekh limit 1 ),
+    query AS (SELECT geom AS geometry FROM borolekh  ),
     q AS (SELECT
         ST_XMin(ST_Collect(geometry)) as x_min,
         ST_XMax(ST_Collect(geometry)) as x_max,
         ST_YMin(ST_Collect(geometry)) as y_min,
         ST_YMax(ST_Collect(geometry)) as y_max,
-        ARRAY_TO_STRING(ARRAY_AGG(CONCAT('<path d="', ST_AsSVG(geometry), '" ', 'fill="green"', ' />')),'') as svg FROM query )
+        ARRAY_TO_STRING(ARRAY_AGG(CONCAT('<path d="', ST_AsSVG(geometry), '" ', 'fill="none"', ' />')),'') as svg FROM query )
 SELECT
-    CONCAT('<svg  height="400" width="400" viewBox="',
+    CONCAT('<svg stroke="black" stroke-solid="3"  height="400" width="400" viewBox="',
         CONCAT_WS(' ', q.x_min, -1 * q.y_max, q.x_max-q.x_min, q.y_max-q.y_min), '">', q.svg, '</svg>') FROM q
